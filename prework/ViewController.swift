67d568fe4c9b62f91d66b073a379bcf51c6095c9
Nikky Lu <nikkylu@foxmail.com>
2021-09-11 21:31:30 -0400

    Initial Commit

---
+++ prework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  prework
+//
+//  Created by Nikky Lu on 2021/9/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
